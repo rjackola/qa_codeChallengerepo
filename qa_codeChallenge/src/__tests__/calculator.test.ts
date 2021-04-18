265e4749884c7f95a6a5047c37d2033ef9f874b5
rjackola <77019533+rjackola@users.noreply.github.com>
2021-04-18 16:09:05 -0400

    Add files via upload

---
+++ qa_codeChallenge/src/__tests__/calculator.test.ts
@@ -0,0 +1,37 @@
+import calculator from "../calculator";
+
+// each of the objects in the dataset array has the pieces of a math problem.
+// "add": x + y
+// "subtract": x - y
+// "multiply": x * y
+// "divide": x / y
+let dataset = [
+  { x: 5, y: 10, method: "add" },
+  { x: 5, y: 10, method: "subtract" },
+  { x: 5, y: 10, method: "multiply" },
+  { x: 5, y: 10, method: "divide" },
+  { x: -12, y: 10000, method: "add" },
+  { x: -12, y: 10000, method: "subtract" },
+  { x: -12, y: 10000, method: "multiply" },
+  { x: -12, y: 10000, method: "divide" },
+  { x: 42, y: 0, method: "add" },
+  { x: 42, y: 0, method: "subtract" },
+  { x: 42, y: 0, method: "multiply" },
+  { x: 42, y: 0, method: "divide" },
+  { x: 81, y: 227, method: "add" },
+  { x: 81, y: 227, method: "subtract" },
+  { x: 81, y: 227, method: "multiply" },
+  { x: 81, y: 227, method: "divide" },
+];
+/* Below is the test to run each equation for the calculator. Remember that method is refering to what equations are being done. */
+ 
+describe("Calculator", () => {});
+  dataset.forEach((calc) => {
+    test(`the ${calc.method} method with ${calc.x} and ${calc.y}`, ()=> {
+      switch (calc.method) {
+        case "add":
+          expect(calculator.add(calc.x, calc.y)).toEqual(calc.x + calc.y);
+      }
+    })
+  })
+/* be sure to ask dre about descirbe and test. Also ask about deleting the node modules and the package json. */
